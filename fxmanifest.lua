64558aaa1c4aadc334404704d4221b5d5f96afb0
Mailar <83369389+MailarYT@users.noreply.github.com>
2021-06-09 15:21:19 +0300

    Watermark uploaded

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Mailar'
+description 'RGB Watermark using HTML and CSS only!'
+
+ui_page 'watermark.html'
+
+files{
+    "watermark.html",
+    "watermark.style.css",
+}
